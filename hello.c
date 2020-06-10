62a4bd2ea71dbf2a3579b722f5cc2740a4291cbc
mic-oss <micoss72@gmail.com>
2020-06-09 21:30:28 -0400

    add asm addition

--- hello.c
+++ hello.c
@@ -7,14 +7,29 @@ int main()
 	int32_t x = 11;
 	int32_t y = 12;
 	int32_t result;
-/*
-   find more at
-    https://gcc.gnu.org/onlinedocs/gcc-10.1.0/gcc/Extended-Asm.html#Extended-Asm
-    */
+
+	/*
+	   find more at
+https://gcc.gnu.org/onlinedocs/gcc-10.1.0/gcc/Extended-Asm.html#Extended-Asm
+	 */
+
+	//source destination ordering
+
 	asm volatile (
 			"imull %%ecx,%%eax;"
 			: "=a" (result)        //output from eax
 			: "a" (x), "c" (y)     //input to eax and ecx
-			);
-	printf("hello asm %d\n", result);
+		     );
+	printf("hello multiplication %d\n", result);
+
+	asm volatile (
+			"addl %%ebx,%%eax;"
+			: "=eax" (result)
+			: "b" (y), "a" (x)
+		     );
+	printf("hello addition %d\n", result);
+
+
+
+
 }
